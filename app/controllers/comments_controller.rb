cd1056136bd01d9b613bc307580673b45ecd3b60
James Almeida <james@peerdustry.com>
2022-03-26 22:54:29 -0300

    Add simple authentication to create articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "James", password: "1234", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
